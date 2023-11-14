0231bfad2d5e01b2ac22a7da79b911a03f9c0744
janne <ejato004@edu.xamk.fi>
2023-11-14 12:33:52 +0200

    modified:   package.json 	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    data: null,
+  };
+
+  componentDidMount() {
+    this.callBackendAPI()
+      .then((res) => this.setState({ data: res }))
+      .catch((err) => console.log(err));
+  }
+
+  callBackendAPI = async () => {
+    try {
+      const response = await fetch('/get_prices');
+      const body = await response.json();
+
+      if (response.status !== 200) {
+        throw Error(body.message);
+      }
+      return body;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      throw error;
+    }
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        {this.state.data !== null ? (
+          <div>
+            <h2>Prices:</h2>
+            <ul>
+              {this.state.data.prices.map((priceData, index) => (
+                <ul key={index}>
+                  <li>Price: {priceData.price}</li>
+                  <li>Start Date: {priceData.startDate}</li>
+                  <li>End Date: {priceData.endDate}</li>
+                </ul>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
